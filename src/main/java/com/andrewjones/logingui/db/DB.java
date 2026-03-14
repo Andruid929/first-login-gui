e0ffce4921c20317795681b2182a9d41a836b1e6
Andrew Jones <princeajsa@gmail.com>
2026-03-15 00:17:02 +0200

    Went super fancy and added HikariCP for connection pooling

--- src/main/java/com/andrewjones/logingui/db/DB.java
+++ src/main/java/com/andrewjones/logingui/db/DB.java
@@ -1,22 +1,28 @@
 package com.andrewjones.logingui.db;
 
-import static com.andrewjones.logingui.db.Secrets.PSQL_PASSWORD;
-import static com.andrewjones.logingui.db.Secrets.PSQL_USER;
+import static com.andrewjones.logingui.db.Secrets.*;
 
-import org.postgresql.ds.PGSimpleDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
 
 public final class DB {
 
-    public static final PGSimpleDataSource DATA_SOURCE;
+    public static final DataSource DATA_SOURCE;
 
     static {
-        DATA_SOURCE = new PGSimpleDataSource();
+        HikariConfig theConfig = new HikariConfig();
+        theConfig.setJdbcUrl(PSQL_URL);
+        theConfig.setUsername(PSQL_USER);
+        theConfig.setPassword(PSQL_PASSWORD);
+        theConfig.setMaximumPoolSize(10);
+        theConfig.setMinimumIdle(2);
+        theConfig.setIdleTimeout(600_000);
+        theConfig.setConnectionTimeout(30_000);
+        theConfig.setMaxLifetime(1_800_000);
 
-        DATA_SOURCE.setDatabaseName(PSQL_USER);
-        DATA_SOURCE.setUser(PSQL_USER);
-        DATA_SOURCE.setPortNumbers(new int[]{12345});
-        DATA_SOURCE.setServerNames(new String[]{"localhost"});
-        DATA_SOURCE.setPassword(PSQL_PASSWORD);
+        DATA_SOURCE = new HikariDataSource(theConfig);
     }
 
 }
